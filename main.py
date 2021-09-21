cc310fa9dcd03d8416e83aecffa0ae536943d7be
Wazzabeee <50945780+Wazzabeee@users.noreply.github.com>
2021-09-21 15:12:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from constants import CONSUMER_SECRET, CONSUMER_KEY, ACCESS_TOKEN_SECRET, ACCESS_TOKEN
+import tweepy as tw
+import json
+
+
+def auth(consumer_key, consumer_secret, access_token, access_token_secret):
+    authentification = tw.OAuthHandler(consumer_key, consumer_secret)
+    authentification.set_access_token(access_token, access_token_secret)
+    return tw.API(authentification, wait_on_rate_limit=True)
+
+
+def json_dump(tweets):
+    path = "C:\\Users\\Clément\\PycharmProjects\\twitter_api\\output"
+
+    with open('data.json', 'w') as outfile:
+        json.dump(tweets, outfile, indent=4)
+
+
+if __name__ == '__main__':
+
+    api = auth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    search_words = "#boruto -filter:retweets"
+    tweets = tw.Cursor(api.search,
+                       q=search_words).items(5)
+
+    dict_tweets = {}
+    dict_tweets['tweets'] = []
+
+    for tweet in tweets:
+        dict_tweets['tweets'].append(tweet._json)
+
+    json_dump(dict_tweets)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
