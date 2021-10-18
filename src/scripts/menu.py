e418d2657e44c76d5e259f807a13d00f53cc5f17
Wazzabeee <50945780+Wazzabeee@users.noreply.github.com>
2021-10-18 10:15:53 +0200

    deleted old menu
    - deleted selection menu as twitter will be the only social media we'll be scraping

--- src/scripts/menu.py
+++ src/scripts/menu.py
@@ -12,28 +12,6 @@ class Menu:
         self.master.iconbitmap("../images/cobweb.ico")
         self.master.title("Scraping tool")
         self.frame = tk.Frame(self.master)
-
-        self.buttons_frame = ttk.Frame(self.frame, borderwidth=1)
-
-        inst = (Image.open('../images/instagram_logo.png'))
-        inst_resized = inst.resize((60, 60), Image.ANTIALIAS)
-        self.real_insta = ImageTk.PhotoImage(inst_resized)
-        self.instagram_button = tk.Button(self.frame, text="Click me", width=60, height=60,
-                                          image=self.real_insta)
-        self.instagram_button["bg"] = "black"
-        self.instagram_button["border"] = "0"
-        self.instagram_button.grid(row=0, column=0)
-
-        img = (Image.open('../images/twitter_logo.png'))
-        resized = img.resize((60, 60), Image.ANTIALIAS)
-        self.real_twitter = ImageTk.PhotoImage(resized)
-        self.twitter_button = tk.Button(self.frame, text="Click me", width=60, height=60,
-                                        image=self.real_twitter,
-                                        command=self.twitter_window())
-        self.twitter_button["bg"] = "black"
-        self.twitter_button["border"] = "0"
-        self.twitter_button.grid(row=0, column=1)
-
         self.frame.pack()
         self.master.update()
         self.center_window()
